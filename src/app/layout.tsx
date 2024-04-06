f70a2483e62d49e68756c8d20d8e8ea42f6854b7
CentralOesteFCO <central.oeste.farmacia@gmail.com>
2024-04-06 17:53:41 -0300

    more styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,14 +13,14 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="es">
-      <body className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
+      <body className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto]">
         <header className="container_header text-xl font-bold leading-[3rem]">
           <Link href="/">
             <h2 className="title_restaurancy">📆 Calendario de Actividades</h2>
           </Link>
         </header>
 
-        <main className="py-8">{children}</main>
+        <main className="px-0 py-8">{children}</main>
 
         <footer className="text-center leading-[3rem] opacity-70">
           © {new Date().getFullYear()} Farmacias Central Oeste
